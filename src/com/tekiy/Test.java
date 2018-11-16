26dd2470686300b7064580195722e2cfa6ac280a
tekiy <tekiy@icloud.com>
2018-11-16 10:23:38 +0800

    tekiy

---
+++ src/com/tekiy/Test.java
@@ -0,0 +1,17 @@
+package com.tekiy;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("new2");
+
+        User user = new User();
+
+        System.out.println("new");
+
+    }
+
+}
