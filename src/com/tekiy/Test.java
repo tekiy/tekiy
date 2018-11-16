3053136712b6bcc84541d8b59078b030dbe79a9a
tekiy <tekiy@icloud.com>
2018-11-16 11:50:33 +0800

    1

--- src/com/tekiy/Test.java
+++ src/com/tekiy/Test.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("new211");
 
         System.out.println("localhost");
+
     }
 
 }
