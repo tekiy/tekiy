6737bb88c7b29b444f021e867d6eff506a11715e
TeKiy <tekiy@icloud.com>
2018-11-16 11:00:47 +0800

    Update Test.java

--- src/com/tekiy/Test.java
+++ src/com/tekiy/Test.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("new2");
-
+        System.out.println("remote");
+        
         User user = new User();
 
+        System.out.println("remote1");
+        
         System.out.println("new");
 
     }
