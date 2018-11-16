c4a719ad002c28ad97b41bc36efd2a66fdc21570
tekiy <tekiy@icloud.com>
2018-11-16 11:09:13 +0800

    Merge branch 'master' of /Users/tekiy/IdeaProjects/tekiy with conflicts.

--- src/com/tekiy/Test.java
+++ src/com/tekiy/Test.java
@@ -7,12 +7,18 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         System.out.println("remote");
-        
+
+        System.out.println("new2");
+
+        System.out.println("loca");
+
         User user = new User();
 
         System.out.println("remote1");
-        
+
         System.out.println("new");
+        user.setAge(11);
+        user.setName("Tekiy");
 
     }
 
